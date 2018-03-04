f3a7e1bfbc26ab6fb33676354be44479c7aeb10f
parishadbehnam <pbehnamghader@yahoo.com>
2018-03-04 15:33:37 +0330

    added mul func to basic.java

--- src/BasicMath.java
+++ src/BasicMath.java
@@ -6,7 +6,9 @@ class BasicMath
         return a + b;
     }
 
-    // TODO: subtract
+   public int mul(int a , int b) { return  a * b;} 
+
+ // TODO: subtract
 	// TODO: multiply
 	// TODO: divide
 }
